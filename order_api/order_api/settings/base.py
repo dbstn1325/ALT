c6efd4fdb4e086e837e46f30a843c24792fbcee1
yoonsu <dbstn1325@naver.com>
2022-08-09 06:59:59 +0900

    mysql container

--- order_api/order_api/settings/base.py
+++ order_api/order_api/settings/base.py
@@ -14,38 +14,13 @@ from email.policy import default
 import pymysql
 pymysql.install_as_MySQLdb()
 
-# env 설정
-import os
-import environ
 
 from pathlib import Path\
 
-# 환경변수 불러올 수 있도록 세팅
-env = environ.Env(DEBUG=(bool, True))
-
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-# env 파일의 위치 알려준다.
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -104,21 +79,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'order_api.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
-
-DATABASES = {
-    'default' : {
-        'ENGINE': 'django.db.backends.mysql',   
-        'NAME': 'altdb',   
-        'USER': 'root',
-        'PASSWORD': '1234',                
-        'HOST': '127.0.0.1',                    
-        'PORT': '3306',                          
-    }
-}
  
 
 # Password validation
---
+++ order_api/order_api/settings/local.py
@@ -0,0 +1,44 @@
+
+# env 설정
+import os
+import environ
+
+from .base import *
+
+# 환경변수 불러올 수 있도록 세팅
+env = environ.Env(DEBUG=(bool, True))
+
+
+# env 파일의 위치 알려준다.
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'altdb',   
+        'USER': 'root',
+        'PASSWORD': '1234',                
+        'HOST': 'localhost',                    
+        'PORT': '3306',                          
+    }
+}
